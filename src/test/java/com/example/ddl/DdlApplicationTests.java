8b55c79eceecdf05d0af9a88171cb4dd27e33bb2
youn-jeong-hoon <yjh5369@wowsoft.com>
2025-04-24 15:19:29 +0900

    feat: DDL 기능 최초 개발

---
+++ src/test/java/com/example/ddl/DdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
